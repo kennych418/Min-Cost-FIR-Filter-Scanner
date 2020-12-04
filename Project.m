6705913ab927cadd8f11c61fbd0fe9c814a4d28b
Kenny Chan <chankenny900@gmail.com>
2020-12-04 02:12:12 -0800

    End night 12/04

--- Project.m
+++ Project.m
@@ -13,16 +13,20 @@ ws = 2*Fs/F_sample; %normalized to pi
 delta_s = 1/A;
 delta_p_decimal = -10^(-0.05*delta_p) + 1; %convert to decimal
 delta_s_decimal = 1/(10^((1/20)*A));  
-%k = delta_p/delta_s;
 
-%% Create and analyze ideal filter
-[PM_estimate,f,a,w] = firpmord([Fp Fs],[1 0],[delta_p_decimal delta_s_decimal],F_sample);
-FIR_ideal = firpm(PM_estimate+9,f,a,w);  %Perform Parks-McClellan filter design
-%FIR_ideal = designfilt('lowpassfir','PassbandFrequency',wp,'StopbandFrequency',ws,'PassbandRipple',delta_p,'StopbandAttenuation',60,'DesignMethod','equiripple');
-%specs_ideal = info(FIR_ideal);
+%% Generate an estimate filter order
+[PM_estimate,f,a,w] = firpmord([Fp Fs],[1 0],[delta_p_decimal delta_s_decimal],F_sample); %Estimate the order
+PM_estimate = PM_estimate + 11; %Provide some margin to optimize
+
+%% Create Ideal Filter
+FIR_ideal = firpm(PM_estimate,f,a,w);  %Perform Parks-McClellan filter design
 [H_ideal, w_ideal] = freqz(FIR_ideal); %Plot ideal filter frequency response
+
+%% Verify Ripple
 [p_max_ripple_ideal,s_max_ripple_ideal] = find_max_ripple(H_ideal,w_ideal,wp*pi,ws*pi); %Verify ripple
-zplane(FIR_ideal); %Plot poles and zeros
+if p_max_ripple_ideal >= delta_p_decimal || s_max_ripple_ideal >= delta_s_decimal
+    fprintf('Ripple does not meet specifications for N = %i\n', PM_estimate);
+end
 
 %% Calculating number of bits
 margin_p = delta_p_decimal - p_max_ripple_ideal;
@@ -36,34 +40,21 @@ n = ceil(n);
 
 %% Quantize coefficients
 FIR_quant_coef = round(FIR_ideal,n);
-
 [H_quant_coef,w_quant_coef] = freqz(FIR_quant_coef);
+
+%% Verify Ripple
 [p_max_ripple_quant_coef,s_max_ripple_quant_coef] = find_max_ripple(H_quant_coef,w_quant_coef,wp*pi,ws*pi); %Verify ripple
-zplane(FIR_quant_coef); %Plot poles and zeros
-plot(FIR_quant_coef);   %plot impulse response
+if p_max_ripple_quant_coef >= delta_p_decimal || s_max_ripple_quant_coef >= delta_s_decimal
+    fprintf('Ripple does not meet specifications for N = %i\n', PM_estimate);
+end
+
+%% Simulate Sine input
+F_input = 3000;
+sig_size = 20000;
+input = 0.5*cos(2*pi*F_input/F_sample*(1:sig_size));
+
+%% Verify SNR
+
 
 %% Calculate Hardware Cost
 cost = 2*(n)^2 + 18*n;
-%% Estimate filter order
-%Kaiser_estimate = ceil((-20*log10(sqrt(delta_s*delta_p))-13)/(14.6*abs(ws-wp)/(2*pi)));
-%Bellanger_estimate = ceil(-2*log10(10*delta_s*delta_p)/(3*abs(ws-wp)/(2*pi))) - 1;
-
-%{
-%% Optimization
-[PM_estimate,f,a,w] = firpmord([Fp Fs],[1 0],[delta_p delta_s],F_sample);
-%f = [0,wp,ws,1]; %Define the stopband and passband
-%a = [1,1,0,0]; %Define the desired amplitude in each band
-%w = [1/k, 1]; %Define the weights in each band
-%if rem(Kaiser_estimate,2) == 0 %Make sure estimate is odd for type I
-%    Kaiser_estimate = Kaiser_estimate+1;
-%end
-[FIR, err]= firpm(PM_estimate+4,f,a,w);  %Perform Parks-McClellan filter design
-%FIR = round(FIR, 5);   %Round filter coefficients to 4 decimal places (4+1 bits)
-%while err < delta_p %Find lowest order filter that meets specs
-%    Kaiser_estimate = Kaiser_estimate - 1;
-%    [FIR, err]= firpm(Kaiser_estimate,f,a);
-%end
-%fprintf('%i\n',Kaiser_estimate + 1); %Best filter order
-figure(1);
-freqz(FIR);
-%}
